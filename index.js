639cb173b7a604704f4fcf089622982635e286c1
TakuyaIshitani <(mokekedasho@icloud.com)>
2017-03-09 01:42:20 +0900

    aタグを追加しました。

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-(function() {
-	var b = document.getElementById('board');
-	console.log(b);
-
-
-})();
-
-
-
-
-
+var b = document.getElementById('board');
+for (var i = 0; i < 8 ; i++) {
+	var tr = document.createElement('tr');
+	var data = [0,0,0,0,0,0,0,0];
+	for (var j = 0; j < 8 ; j++) {
+		var td = document.createElement('td');
+		td.className = 'cell';
+		td.id = 'cell' + i + j;
+		var a = document.createElement('a');
+		a.className ='piece';
+		a.id = 'piece' + i + j;
+		var piece = document.getElementById("piece");
+		//piece.innerHTML = "○";
+		//td.onclick = clicked;
+		tr.appendChild(td);
+		td.appendChild(a);
+	}
+	b.appendChild(tr);
+}
 
