23b0b6a6e01aab8759ec213bb65cb806db2a6de0
14ta98 <14ta98@IshitaniTakuya-no-MacBook-Air.local>
2017-06-01 14:22:46 +0900

    盤に駒を配置しました。

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 /* 初期設定 */
-
 // 駒を定義する処理
 var empty = 0;
 var black = 1;
 var white = 2;
+var board_data = [];
 
 // 盤の中身を配列で初期化する処理
-function InitialBoardData() {
-	var board_data = [];
+function InitBoardData () {
 	for (var i = 0; i < 8; i++) {
 		board_data[i] = [];
 		for (var j = 0; j < 8; j++) {
@@ -21,4 +20,25 @@ function InitialBoardData() {
 	board_data[5][4] = 2;
 }
 
-InitialBoardData();
+InitBoardData();
+
+// 盤に駒を置く処理
+function put (board_data,i,j,color) {
+	if (check(board_data,i,j,color) === false) {
+		return false;
+	}
+	board_data[i][j] = color;
+}
+
+// 盤に駒を置けるかチェックする処理
+function check(board_data,i,j,color){
+  //すでに石がある場合
+  if(board_data[i][j] != 0){
+    return false;
+  }
+  //挟めない場合
+  if(board_data[i][j]){
+    false;
+  }
+  return true;
+}
