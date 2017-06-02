53d7a3d3c0262b8a639ce04b21abccd3510ec967
TakuyaIshitani <(mokekedasho@icloud.com)>
2017-06-03 01:05:12 +0900

    盤をクリックすると駒が配置される処理を追加しました。

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 var empty = 0;
 var black = 1;
 var white = 2;
-
+var board_data = [];
+var piece = "";
 // 盤の中身を配列で初期化する処理
 function InitialBoardData() {
-	var board_data = [];
 	for (var i = 0; i < 8; i++) {
 		board_data[i] = [];
 		for (var j = 0; j < 8; j++) {
@@ -19,57 +19,61 @@ function InitialBoardData() {
 	board_data[3][3] = 1;
 	board_data[4][3] = 2;
 	board_data[3][4] = 2;
-
-	//tableにidを設定する
+	//盤にidを設定する
 	var table = document.getElementById("board");
 	for(var i = 0; i < table.rows.length; i++){
 		for(var j = 0; j < table.rows[i].cells.length; j++){
 			table.rows[i].cells[j].id = "cell" + i + j;
 		}
 	}
-
 	//idごとに値を設定する
-    var test = "";
 	for (var i = 0; i < 8; i++) {
 		for (var j = 0; j < 8; j++) {
 			if(board_data[i][j] == empty){
-				test = "";
+				piece = "";
 			}
-			else if(board_data[i][
-				j] == black){
-				test = "●";
+			else if(board_data[i][j]
+				 == black){
+				piece = "●";
 			}
 			else if(board_data[i][j] == white){
-				test = "○";
+				piece = "○";
 			}
 			else{
-				test = "エラー";
+				piece = "エラー";
 			}
-			document.getElementById("cell" + i + j).textContent = test;
+			document.getElementById("cell" + i + j).textContent = piece;
 		}
 	};
 }
 
+//盤に駒を配置する処理
 function put(i,j,color){
-　//石が置けるかチェックする
+　//盤に駒が置けるかチェックする処理
   if(check(i,j,color) == false){
     return false;
   }
+  //駒をひっくり返す処理
   board_data[i][j] = color;
-  //ひっくり返す処理
 
 }
-
+//盤に駒があるかチェックする処理
 function check(i,j,color){
   //すでに石がある場合
   if(board_data[i],[j] != 0){
     return false;
   }
-  //挟めない場合
-  if(false){
-    false;
-  }
-
   return true;
+}
 
+//盤をクリックした際の処理
+function setMark(obj) {
+	for (var i = 0; i < 8; i++) {
+		for (var j = 0; j < 8; j++) {
+			//盤に駒がない場合の処理
+			if(board_data[i][j] == empty){
+				obj.textContent = "●";
+			}
+		}
+	}
 }
