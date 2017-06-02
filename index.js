e19173b879ad48bb39c64864739438d1bcc5f793
14ta98 <14ta98@IshitaniTakuya-no-MacBook-Air.local>
2017-06-02 16:24:19 +0900

    駒を初期配置しました。

--- index.js
+++ index.js
@@ -1,44 +1,75 @@
 /* 初期設定 */
+
 // 駒を定義する処理
 var empty = 0;
 var black = 1;
 var white = 2;
-var board_data = [];
 
 // 盤の中身を配列で初期化する処理
-function InitBoardData () {
+function InitialBoardData() {
+	var board_data = [];
 	for (var i = 0; i < 8; i++) {
 		board_data[i] = [];
 		for (var j = 0; j < 8; j++) {
-		board_data[i][j] = empty;
+			board_data[i][j] = empty;
 		}
 	}
 	// 初期配置
 	board_data[4][4] = 1;
-	board_data[5][5] = 1;
-	board_data[4][5] = 2;
-	board_data[5][4] = 2;
-}
+	board_data[3][3] = 1;
+	board_data[4][3] = 2;
+	board_data[3][4] = 2;
 
-InitBoardData();
-
-// 盤に駒を置く処理
-function put (board_data,i,j,color) {
-	if (check(board_data,i,j,color) === false) {
-		return false;
+	//tableにidを設定する
+	var table = document.getElementById("board");
+	for(var i = 0; i < table.rows.length; i++){
+		for(var j = 0; j < table.rows[i].cells.length; j++){
+			table.rows[i].cells[j].id = "cell" + i + j;
+		}
 	}
-	board_data[i][j] = color;
+
+	//idごとに値を設定する
+    var test = "";
+	for (var i = 0; i < 8; i++) {
+		for (var j = 0; j < 8; j++) {
+			if(board_data[i][j] == empty){
+				test = "";
+			}
+			else if(board_data[i][
+				j] == black){
+				test = "●";
+			}
+			else if(board_data[i][j] == white){
+				test = "○";
+			}
+			else{
+				test = "エラー";
+			}
+			document.getElementById("cell" + i + j).textContent = test;
+		}
+	};
 }
 
-// 盤に駒を置けるかチェックする処理
-function check(board_data,i,j,color){
+function put(i,j,color){
+　//石が置けるかチェックする
+  if(check(i,j,color) == false){
+    return false;
+  }
+  board_data[i][j] = color;
+  //ひっくり返す処理
+
+}
+
+function check(i,j,color){
   //すでに石がある場合
-  if(board_data[i][j] != 0){
+  if(board_data[i],[j] != 0){
     return false;
   }
   //挟めない場合
-  if(board_data[i][j]){
+  if(false){
     false;
   }
+
   return true;
+
 }
