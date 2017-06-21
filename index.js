298661899074949978d26a8307b3e777d0fad31d
TakuyaIshitani <(mokekedasho@icloud.com)>
2017-06-21 22:02:54 +0900

    ソースを整理しました。

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function InitialBoardData() {
 
 //盤に駒を配置する処理
 function put(i,j,color){
-　//盤に駒が置けるかチェックする処理
+	//盤に駒が置けるかチェックする処理
   if(check(i,j,color) == false){
     return false;
   }
@@ -66,34 +66,73 @@ function check(i,j,color){
   return true;
 }
 
-//盤に駒がある場合の処理
-// fucntion existPiece(i,j,color) {
-// 	if (data[i][j] === black || data[i][j] === white) {
-// 		//return true;
-// 	}
-// }
-
-
 //盤をクリックした際の処理
-function setMark(obj) {
-	// for (var i = 0; i < 8; i++) {
-	// 	for (var j = 0; j < 8; j++) {
-			//盤に駒がない場合の処理
+function setMark(obj,color) {
+	var cellid = obj.id;
+	var px = parseInt(cellid.slice(4,5));
+	var py = parseInt(cellid.slice(5,6));
+	var bx = -1
+	var by = -1;
+	var dx = +1;
+	var dy = 0;
+	var x = px + dx;
+	var y = py + dy;
+	console.log(x);
+	console.log(y);
+	var f = false;
 
-			var cellid = obj.id;
 
-			var first = cellid.slice(4,5);
-			var second = cellid.slice(5,6);
-			if(board_data[first][second] == empty){
-				obj.textContent = "●";
-				//自分の置いたところから、横と縦と斜めのセルを確認する処理を記述する
-				//考え方:隣を一個づつ監視する
-				//
-			}
-
+	if(board_data[px][py] === empty){
+	obj.textContent = "●";
+	} 
 }
 
+	// 	if (board_data[x][y] === empty) {
+	// 	break;
+	// } else if (board_data[x][y] === white) {
+	// 	f = true;
+	// } else if (board_data[x][y]) === black {
+	// 	bx = x;
+	// 	by = y;
+	// 	x = x+dx;
+	// 	y = y+dy;
+	// }
 
+	// 
+
+	// if (x < 0 || y < 0 || x > 7 || y > 7) {
+	// 	board_data[x][y] === empty;
+	// 	break;
+	// } else if (board_data) {}
+
+	//自分のおいた駒から、縦と横と斜めのセルを確認する処理を記述する
+	// var dirs = [[-1,-1],[0,-1],[1,-1],[-1,0],[1,0],[-1,1],[0,1],[1,1]];
+	// var result = [];
+	// for (var p = 0; i < dirs.length; p++) {
+	// 	var flipped = getFlipCellsOneDir(i, j, dirs[p],[0], dirs[p][i], color);
+	// 	result = result.concat(flipped);
+
+// // i,jに駒を置いた時に、（dx,dy）方向で駒を挟めるかを確認する処理
+// 	var x = i + dx;
+// 	var y = j + dy;
+// 	var flipped = [];
+
+// if (x < 0 || y < 0 || x > 7 || y > 7 || 
+// 	data[x][y] == color ||  data[x][y] === 0){
+// 		return;
+// 	}
+// flipped.push([x,y]);
+
+// while (true) {
+// 	x += dx;
+// 	y += dy;
+// 	if (x < 0 || y < 0 || y > 7 || data[x][y] === 0) {
+// 		return true;
+// 	}
+// 	if (data[x][y] === color) {
+// 		return flipped
+// 	}
+// }
 
 
 
