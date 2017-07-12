ed1eb5d69de619a4c2423ee693fd368348767e38
TakuyaIshitani <(mokekedasho@icloud.com)>
2017-07-12 16:07:14 +0900

    latest commit

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 /* 初期設定 */
 var empty = 0;
+// 駒の色を定義する処理
 var black = 1;
 var white = 2;
-var board_data = [];
-var piece = "";
+// 次に配置する駒の色を定義する処理
 var nextColor = 0;
+// 駒の中身を初期化する処理
+var board_data = [];
+// 駒の描写を初期化する処理
+var piece = "";
+// 盤のidを初期化する処理
 var cellid = '';
+// 自分が配置する駒の座標を初期化する処理
 var myPut_x = -1;
 var myPut_y = -1;
-//　ひっくり返す最初の駒	
+//　ひっくり返す最初の駒を定義する処理	
 var end_x = -1
 var end_y = -1;
-// 盤を確認する方向
+// 盤を確認する方向を定義する処理
 var dir_x = -1;
 var dir_y = -1;
+// 駒をひっくり返す際の対象である駒の位置を初期化する処理
 var target_x = -1;
 var target_y = -1;
+
+
 var changeFlag = false;
-var skipFlag = true;
+var skipFlag = false;
 var checkFlag = false;
 
 // 盤の中身を配列で初期化する処理
 function InitialBoardData() {
 	for (var i = 0; i < 8; i++) {
-		// *board_data[j]は初期化しなくていいの？
 		board_data[i] = [];
 		for (var j = 0; j < 8; j++) {
 			board_data[i][j] = empty;
@@ -72,10 +80,10 @@ function InitialBoardData() {
 
 // 変数初期化
 function init () {
-	//　自分が配置した駒に対する盤（セル）のidを取得する処理　
+	// 自分が配置した駒に対する盤（セル）のidを取得する処理　
 	myPut_x = parseInt(cellid.slice(4,5));
 	myPut_y = parseInt(cellid.slice(5,6));
-	//  駒をひっくり返す際の対象である駒の位置を取得する処理
+	// 駒をひっくり返す際の対象である駒の位置を取得する処理
 	target_x = myPut_x + dir_x;
 	target_y = myPut_y + dir_y;
 	//　ひっくり返す駒の末端を取得する処理
@@ -87,19 +95,20 @@ function init () {
 
 // 盤をクリックした時の処理
 function setMark(obj) {
-	//　クリックした盤（セル）のidを取得する処理
+	// クリックした盤（セル）のidを取得する処理
 	cellid = obj.id;
 	// 実際にひっくり返す処理を行うため、checkFlagをfalseにする
 	checkFlag = false;
 	init();
 	// 盤の中身が空の場合に実行する処理
 	if(board_data[myPut_x][myPut_y] === empty){
-		
+		//　次に配置する駒が黒の場合に行う処理
 		if (nextColor === black) {
 			obj.textContent = "●";
 			//　自分が配置した盤の中身を黒にする処理
 			board_data[myPut_x][myPut_y] = black;
 		} else {
+			//次に配置する駒が白の場合に行う処理
 			obj.textContent = "○";
 			//　自分が配置した盤の中身を白にする処理
 			board_data[myPut_x][myPut_y] = white;
@@ -126,11 +135,12 @@ function setMark(obj) {
 
 	} else {
 		alert('既に駒が配置されています。');
-	}
+	} 
+	skipFlag = false;
 }	
 
 function set() {
-	// 確認している対象の駒が盤上の場合に行う処理
+	// 確認している対象の駒が盤上にある場合に行う処理
 	while(0 <= target_x && target_x <= 7 && 0 <= target_y && target_y <= 7 ) {
 		// 盤上に挟む駒がない場合の処理
 		if (board_data[target_x][target_y] === empty) {
@@ -151,43 +161,11 @@ function set() {
 			
 		}
 	} 
-
-	// // 	チェックモードの場合
-	// if(checkFlag){
-	// 	if(changeFlag){
-	// 		document.getElementById(cellid).style.backgroundColor　= '#33CC33';
-	// 		skipFlag = false;
-	// 		return;
-	// 	}
-	// }
-	// //	ひっくり返す処理の場合
-	// else{
-	// 	if(changeFlag){
-	// 		var x = myPut_x;
-	// 		var y = myPut_y;
-	// 		//  末端までひっくり返す
-	// 		do {
-	// 			//　自分の置いた駒の隣から順にend_x,end_yまで駒をひっくり返す
-	// 			x = x + dir_x;
-	// 			y = y + dir_y;
-	// 			board_data[x][y] = nextColor;
-	// 			if (nextColor === black) {
-	// 				document.getElementById('cell' + x + y).textContent = "●";
-	// 			} else {
-	// 				document.getElementById('cell' + x + y).textContent = "○";
-	// 			}
-	// 		} while (x != end_x || y != end_y)
-	// 	}
-	// }
-	// チェックフラグみてからのコードに修正する！
-
-
-
 	//配置できる場所を確認する処理
 	if (checkFlag && changeFlag) {
 		document.getElementById(cellid).style.backgroundColor　= '#33CC33';
-		skipFlag = false;
-		return;
+		//skipFlag = false;
+		return false;
 	}
 	//ループ終了後に、f が true ならば（はさんだ相手ゴマがある）、盤面[px+dx][py+dy]～[bx][by]までを自分のコマにする
 	else if (changeFlag) {
@@ -207,49 +185,57 @@ function set() {
 		} while (x != end_x || y != end_y)
 		skipFlag = false;
 	}
+
+	return true;
 }
 
 function checkBoard() {
+	var isSkip;
 	//　右方向確認
 	dir_x = +1;
 	dir_y = 0;
 	init();
-	set();
+	isSkip &= set();
 	// 左方向確認
 	dir_x = -1;
 	dir_y = 0;
 	init();
-	set();
+	isSkip &= set();
 	// 下方向確認
 	dir_x = 0;
 	dir_y = +1;
 	init();
-	set();
+	isSkip &= set();
 	// 上方向確認
 	dir_x = 0;
 	dir_y = -1;
 	init();
-	set();
+	isSkip &= set();
 	//　斜め右下
 	dir_x = +1;
 	dir_y = +1;
 	init();
-	set();
+	isSkip &= set();
 	//	斜め右上
 	dir_x = +1;
 	dir_y = -1;
 	init();
-	set();
+	isSkip &= set();
 	//	斜め左下
 	dir_x = -1;
 	dir_y = +1;
 	init();
-	set();
+	isSkip &= set();
 	//  斜め左上
 	dir_x = -1;
 	dir_y = -1;
 	init();
-	set();
+	isSkip &= set();
+
+	if(isSkip){
+		skipFlag = true;
+	}
+
 }
 //配置できる場所の確認
 function isCheck () {
@@ -271,9 +257,7 @@ function isCheck () {
 			};
 		}
 	}
-
 }
-
 
 function test(){
 	//　skipの場合は相手のターンになる
@@ -283,6 +267,6 @@ function test(){
 		} else if (nextColor === white) {
 			nextColor = black;
 		} 
-		alert(nextColor + "の配置できる場所がアリません。");
+		alert("駒を配置できません。");	
 	}
 }
